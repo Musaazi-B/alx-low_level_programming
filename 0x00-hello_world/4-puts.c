c2c2f7d38651c0fe5fe4887d3140cc23c5c61dc7
Musaazi-B <brianbricom@outlook.com>
2022-06-30 10:05:17 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/** that prints exactly "Programming is like building a multiligual puzzle", followed by a new line.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/** that prints exactly "with proper grammar, but the outcome is a piece of art", followed by a new line.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
